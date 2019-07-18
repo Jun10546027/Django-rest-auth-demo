d7c44747ac11f8c04043ee74042a2093e7c73e60
10546027Jun <10546027@ntub.edu.tw>
2019-07-18 15:21:00 +0800

    設定email欄位，避免建造superuser時會產生的錯誤

--- test_token/models.py
+++ test_token/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     created = models.DateTimeField(auto_now_add=True,verbose_name="建立時間")
     gender=models.CharField(max_length=1,verbose_name="性別")
-    # email = models.EmailField(unique=True)
-    REQUIRED_FIELDS = ["gender"]
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS = ["gender","email"]
 
     class Meta:
         ordering = ('created',)
